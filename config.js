d4d0f34172cf00aaa4344813f2f7a6bd11959d97
maheshvemudala <vemudala.mahesh@lntecc.com>
2022-09-06 18:10:31 +0530

    added webhook

--- config.js
+++ config.js
@@ -12,7 +12,7 @@
 "access_token":null,
 "tokenTime":null,
 "adapterApi":{​
-"url":"https://smartpatna.quantela.com/qpa/1.0.0/webhooks/smartpatna.com/paboxwebhook",
+"url":"https://smartpatna.quantela.com/quanteladataapi/webhook/L1ME6IIBH5tH8P7qCSYl",
 "method":"POST",
 "headers":{​"Content-Type":"application/json", "apikey":"478d5225-0d88-4910-b928-595df5055384"}​,
 "data":{​}​,
